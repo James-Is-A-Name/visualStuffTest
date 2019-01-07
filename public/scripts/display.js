a200215402f6fe7943139ff1b15903d816a19e89
James <james-is-a-name@users.noreply.github.com>
2019-01-07 15:44:53 +1300

    add movment calculation of objects and rendering using html element manipulation

--- public/scripts/display.js
+++ public/scripts/display.js
@@ -7,6 +7,11 @@ let mainDisplay;
 function setupDisplayArea(){
     mainDisplay = new display();
 
+    let loop = setInterval(() => {
+        mainDisplay.moveItems()
+        mainDisplay.redrawItems()
+    }
+    ,20)
 }
 
 function speak(message){
@@ -26,9 +31,17 @@ class display{
         this.drawArea();
 
         this.items = [];
-        this.items.push(new visualItem(10,10,30,40));
+        this.items.push(new visualItem(10,10,30,40,this.items.length));
+        this.items.push(new visualItem(101,110,60,40,this.items.length));
+        this.items.push(new visualItem(103,210,30,40,this.items.length));
+        this.items.push(new visualItem(120,310,30,40,this.items.length));
+        this.items.push(new visualItem(410,410,30,40,this.items.length));
 
-        this.drawItems()
+        [1,2,3,4,5,6,7,8,9,10,11,12,13,1,1,1,1,1,1,1,1,1,1,1,1,1,1].forEach( (val,i)=> {
+            this.items.push(new visualItem(10*val,10+i*5,30+val,50-val,this.items.length));
+        })
+
+        this.setupItems()
     }
 
     drawArea(){
@@ -44,30 +57,88 @@ class display{
         displayElement.style.height = this.height + "px";
     }
 
-    drawItems(){
+    setupItems(){
         let displayElement = document.getElementById("displayElement");
 
         speak(`items length is ${this.items.length}`)
 
-        this.items.forEach(element => {
+        this.items.forEach(itemData => {
 
-            speak(`one item`)
+            if(itemData.itemId >= 0){
+                let anItem = document.createElement("div");
 
-            let anItem = document.createElement("div");
+                anItem.style.left = itemData.x + "px";
+                anItem.style.top = itemData.y + "px";
 
-            anItem.style.left = element.x + "px";
-            anItem.style.top = element.y + "px";
+                anItem.style.width = itemData.width + "px";
+                anItem.style.height = itemData.height + "px";
 
-            anItem.style.width = element.width + "px";
-            anItem.style.height = element.height + "px";
+                anItem.style.position = "absolute"
 
-            anItem.style.position = "absolute"
+                anItem.style.border = "dashed";
 
-            anItem.style.border = "dashed";
+                anItem.id = "item-"+itemData.itemId;
 
-            //This will just keep adding things so not the best
-            displayElement.appendChild(anItem);
+                //This will just keep adding things so not the best
+                displayElement.appendChild(anItem);
+            }
         });
+    }
+
+    moveItems(){
+        this.items = this.items.map( itemData=> {
+            itemData.x += itemData.xChange * itemData.xPolarity;
+            itemData.y += itemData.yChange * itemData.yPolarity;
+
+            if(itemData.y < 0){
+                itemData.y = 0;
+                itemData.yPolarity = 1;
+            }
+            else if(itemData.y >= this.height - itemData.height){
+
+                itemData.y = this.height - itemData.height - 1;
+                itemData.yPolarity = -1;
+                
+                if(Math.random() > 0.5){
+                    itemData.xChange = Math.floor(Math.random() * 10 + 1)
+                    itemData.yChange = Math.floor(Math.random() * 10 + 1)
+
+                    if(Math.random() > 0.90){
+                        itemData.xPolarity *= -1;
+                    }
+                }
+            }
+
+            if(itemData.x < 0){
+                itemData.x = 0;
+                itemData.xPolarity = 1;
+            }
+            else if(itemData.x >= this.width - itemData.width){
+                itemData.x = this.width - itemData.width - 1;
+                itemData.xPolarity = -1;
+            }
+
+            return itemData;
+        })
+    }
+
+    redrawItems(){
+        let displayElement = document.getElementById("displayElement");
+
+        let childElemnts = displayElement.childNodes;
+
+        childElemnts.forEach( itemInfo =>{
+            let id = itemInfo.id
+            
+            let item = document.getElementById(id);
+            
+            let itemData = this.items[id.substring(5)];
+
+            item.style.left = itemData.x + "px";
+            item.style.top = itemData.y + "px";
+            item.style.width = itemData.width + "px";
+            item.style.height = itemData.height + "px";
+        })
     }
 }
 
@@ -75,11 +146,16 @@ class display{
 
 
 class visualItem{
-    constructor(x = 100,y = x, width = 10, height = width){
+    constructor(x = 100,y = x, width = 10, height = width, itemId = -1){
 
+        this.itemId = itemId;
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.xChange = 1;
+        this.yChange = 1;
+        this.xPolarity = 1;
+        this.yPolarity = 1;
     }
 }
