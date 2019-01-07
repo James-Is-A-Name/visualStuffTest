c5f0bfa19160b683523d1f2fc38a1cab0279975e
James <james-is-a-name@users.noreply.github.com>
2019-01-07 14:16:14 +1300

    add simple item handling

--- public/scripts/display.js
+++ public/scripts/display.js
@@ -26,6 +26,9 @@ class display{
         this.drawArea();
 
         this.items = [];
+        this.items.push(new visualItem(10,10,30,40));
+
+        this.drawItems()
     }
 
     drawArea(){
@@ -33,10 +36,50 @@ class display{
         
         speak("testing an error");
 
-        displayElement.style.border = "solid"
+        displayElement.style.border = "solid";
 
-        displayElement.style.width = this.width + "px"
-        displayElement.style.height = this.height + "px"
+        displayElement.style.position = "relative";
+
+        displayElement.style.width = this.width + "px";
+        displayElement.style.height = this.height + "px";
+    }
+
+    drawItems(){
+        let displayElement = document.getElementById("displayElement");
+
+        speak(`items length is ${this.items.length}`)
+
+        this.items.forEach(element => {
+
+            speak(`one item`)
+
+            let anItem = document.createElement("div");
+
+            anItem.style.left = element.x + "px";
+            anItem.style.top = element.y + "px";
+
+            anItem.style.width = element.width + "px";
+            anItem.style.height = element.height + "px";
+
+            anItem.style.position = "absolute"
+
+            anItem.style.border = "dashed";
+
+            //This will just keep adding things so not the best
+            displayElement.appendChild(anItem);
+        });
     }
 }
 
+
+
+
+class visualItem{
+    constructor(x = 100,y = x, width = 10, height = width){
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
